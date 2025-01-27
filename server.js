8f16985dfc4e1865dbe6821d0298f4d2da48fe3a
RickLeinecker2 <rick2@jsventures.com>
2025-01-27 13:52:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -188,7 +188,7 @@ app.post('/api/searchcards', async (req, res, next) =>
     _ret.push( results[i].Card );
   }
   
-  var ret = {results:_ret, error:error,cicid:"test123"};
+  var ret = {results:_ret, error:error,cicid:"test123123123"};
   res.status(200).json(ret);
 });
 
